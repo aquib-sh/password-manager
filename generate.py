343adf764b24127b565b8db7207fa1e255b395d3
aquib-sh <shaikhaquib394@gmail.com>
2022-03-04 16:50:37 +0530

    Add script for generation of Google API token

---
+++ generate.py
@@ -0,0 +1,10 @@
+# Script to generate a Google token
+import config
+from services.google.token import TokenGenerator
+
+gtr = TokenGenerator()
+creds = gtr.generate_google_api_token(
+        client_secret=config.client_secret_path,
+        scopes=config.google_api_scope)
+
+gtr.export_token(creds, config.google_api_token_path)
