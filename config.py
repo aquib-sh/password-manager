c10367afa91d2859bc65f53404b11b32df9adf47
aquib-sh <shaikhaquib394@gmail.com>
2022-03-04 16:27:36 +0530

    Add configuration file

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+data_dir = os.path.join(os.getcwd(), "internal")
+# ======================= CACHE FILE  =============================
+cache_file = "cache.json"
+cache_path = os.path.join(data_dir, cache_file)
+# ======================= GOOGLE APIs =============================
+google_token_file  = "final_token.json"
+client_secret_file = "client_secret.json"
+
+client_secret_path    = os.path.join(data_dir, client_secret_file)
+google_api_token_path = os.path.join(data_dir, google_token_file)
+
+google_api_scope = ["https://mail.google.com/auth/gmail.modify"]
