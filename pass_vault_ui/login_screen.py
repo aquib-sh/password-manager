b863b486ac74ebe0b0386f2a8a398da233d2385a
aquib-sh <shaikhaquib394@gmail.com>
2022-03-05 22:07:17 +0530

    Add LoginWindow module

---
+++ pass_vault_ui/login_screen.py
@@ -0,0 +1,17 @@
+from PyQt5.QtWidgets import QMainWindow
+from pass_vault_ui.layouts import login_ui
+
+class LoginWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.__login_ui = login_ui.Ui_Form()
+        self.__login_ui.setupUi(self)
+
+    def connect_login_btn(self, event):
+        self.__login_ui.login_btn.clicked.connect(event)
+
+    def connect_reset_btn(self, event):
+        self.__login_ui.reset_btn.clicked.connect(event)
+
+    def connect_register_btn(self, event):
+        self.__login_ui.register_btn.clicked.connect(event)
