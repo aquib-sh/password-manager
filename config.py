31baedef467a00ebeea455efb28e99dc344286f6
aquib-sh <shaikhaquib394@gmail.com>
2022-03-05 10:17:52 +0530

    Add format for OTP, add spacing between sections

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ data_dir = os.path.join(os.getcwd(), "internal")
 # ======================= CACHE FILE  =============================
 cache_file = "cache.json"
 cache_path = os.path.join(data_dir, cache_file)
+
 # ======================= GOOGLE APIs =============================
 google_token_file  = "final_token.json"
 client_secret_file = "client_secret.json"
@@ -12,3 +13,6 @@ client_secret_path    = os.path.join(data_dir, client_secret_file)
 google_api_token_path = os.path.join(data_dir, google_token_file)
 
 google_api_scope = ["https://www.googleapis.com/auth/gmail.modify"]
+
+# ====================== FORMAT FOR OTP ===========================
+OTP_MSG = "Hello {user} your OTP for Pass Vault is {otp}"
