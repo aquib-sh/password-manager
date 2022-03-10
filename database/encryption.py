9dcb360d5fedfd235f3e2f18c35113c86a371560
Melville <melville2000.md@gmail.com>
2022-03-10 22:38:23 +0530

    Add Encryption module

---
+++ database/encryption.py
@@ -0,0 +1,21 @@
+from cryptography.fernet import Fernet
+
+
+class CryptEngine():
+
+    def generate_key(self):
+        key  = Fernet.generate_key()
+        return key
+
+    def encrypt_message(self,message):
+        encoded_message = message.encode()
+        f = Fernet(key)
+        encrypted_message = f.encrypt(message)
+        return encrypted_message
+
+    def decrypt_message(self,key,message):
+        encoded_message = message.encode()
+        f = Fernet(key)
+        decrypted_message = f.decrypt(message)
+        return decrypted_message
+
