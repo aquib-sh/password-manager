bba06c146acad6bcecd4c3af7185710706164596
aquib-sh <shaikhaquib394@gmail.com>
2022-03-18 21:23:19 +0530

    Fix parameters, add keyas a parameter to encrypt_message()

--- database/encryption.py
+++ database/encryption.py
@@ -7,13 +7,13 @@ class CryptEngine():
         key  = Fernet.generate_key()
         return key
 
-    def encrypt_message(self,message):
+    def encrypt_message(self, message, key):
         encoded_message = message.encode()
         f = Fernet(key)
         encrypted_message = f.encrypt(message)
         return encrypted_message
 
-    def decrypt_message(self,key,message):
+    def decrypt_message(self, message, key):
         encoded_message = message.encode()
         f = Fernet(key)
         decrypted_message = f.decrypt(message)
