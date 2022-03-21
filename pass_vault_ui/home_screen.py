27ef00b1cc3ffe4d6aec2a8986fa33f444dafed2
aquib-sh <shaikhaquib394@gmail.com>
2022-03-21 14:39:54 +0530

    make home_ui a public variable

--- pass_vault_ui/home_screen.py
+++ pass_vault_ui/home_screen.py
@@ -4,14 +4,14 @@ from pass_vault_ui.layouts import home_ui
 class HomeWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.__home_ui = home_ui.Ui_MainWindow()
-        self.__home_ui.setupUi(self) 
+        self.home_ui = home_ui.Ui_MainWindow()
+        self.home_ui.setupUi(self) 
 
     def connect_logout_btn(self, event):
-        self.__home_ui.log_out_btn.clicked.connect(event)
+        self.home_ui.log_out_btn.clicked.connect(event)
 
     def connect_add_btn(self, event):
-        self.__home_ui.add_btn.clicked.connect(event)
+        self.home_ui.add_btn.clicked.connect(event)
 
     def connect_delete_btn(self, event):
-        self.__home_ui.delete_btn.clicked.connect(event)
+        self.home_ui.delete_btn.clicked.connect(event)
