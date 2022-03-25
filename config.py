2352193b0c310d3ee6b510d95f07e33f53d62b65
aquib-sh <shaikhaquib394@gmail.com>
2022-03-25 20:02:01 +0530

    Add info about database file

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
+# ======================= DATABASE FILE  =============================
 data_dir = os.path.join(os.getcwd(), "internal")
-# ======================= CACHE FILE  =============================
-cache_file = "cache.json"
-cache_path = os.path.join(data_dir, cache_file)
+db_file = "password_manager.db"
+db_path = os.path.join(data_dir, db_file)
 
 # ======================= GOOGLE APIs =============================
 google_token_file  = "final_token.json"
