35792249170eecd9730bede37eaeb333fdeba476
aquib-sh <shaikhaquib394@gmail.com>
2022-03-26 13:32:34 +0530

    change to UPPERCASE variables and add email

--- config.py
+++ config.py
@@ -1,18 +1,25 @@
 import os
 
 # ======================= DATABASE FILE  =============================
-data_dir = os.path.join(os.getcwd(), "internal")
-db_file = "password_manager.db"
-db_path = os.path.join(data_dir, db_file)
+DATA_DIR = os.path.join(os.getcwd(), "internal")
+DB_FILE = "password_manager.db"
+DB_PATH = os.path.join(DATA_DIR, DB_FILE)
 
 # ======================= GOOGLE APIs =============================
-google_token_file  = "final_token.json"
-client_secret_file = "client_secret.json"
+GOOGLE_TOKEN_FILE  = "final_token.json"
+CLIENT_SECRET_FILE = "client_secret.json"
 
-client_secret_path    = os.path.join(data_dir, client_secret_file)
-google_api_token_path = os.path.join(data_dir, google_token_file)
+CLIENT_SECRET_PATH    = os.path.join(DATA_DIR, CLIENT_SECRET_FILE)
+GOOGLE_API_TOKEN_PATH = os.path.join(DATA_DIR, GOOGLE_TOKEN_FILE)
 
-google_api_scope = ["https://www.googleapis.com/auth/gmail.modify"]
+GOOGLE_API_SCOPE = ["https://www.googleapis.com/auth/gmail.modify"]
 
+APP_EMAIL = "passvault.manager@gmail.com"
 # ====================== FORMAT FOR OTP ===========================
 OTP_MSG = "Hello {user} your OTP for Pass Vault is {otp}"
+
+# ====================== DISPLAY MSGS ========================
+ENTER_OTP_MSG = "OTP has been sent to {email},\nPlease enter OTP here."
+INCORRECT_OTP_MSG = "Incorrect OTP!"
+REG_SUCESS_MSG = "Sucessfully Registered on PassVault!,\nPlease close this window and proceeed with login"
+PASSWORD_MISMATCH_MSG = "Passwords do not match, Please check the passwords and try again."
