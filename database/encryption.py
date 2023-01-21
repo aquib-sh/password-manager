8c0a98e41f069c424abfd5a0043e2c86acdc0389
Aquib <shaikhaquib394@gmail.com>
2023-01-21 19:59:39 +0530

    Decode the encrypted string while returning

--- database/encryption.py
+++ database/encryption.py
@@ -14,7 +14,7 @@ class CryptEngine(object):
         raw = self._pad(raw)
         iv = Random.new().read(AES.block_size)
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return base64.b64encode(iv + cipher.encrypt(raw.encode()))
+        return base64.b64encode(iv + cipher.encrypt(raw.encode())).decode()
 
     def decrypt(self, enc):
         enc = base64.b64decode(enc)
